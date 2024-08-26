fcb73264e8e44746d164cfaba4928badc750b236
Sugam Raj Prasai <sugamprasai9818@gmail.com>
2024-08-26 21:16:03 +0545

    somechanges

--- insert_transaction.php
+++ insert_transaction.php
@@ -1,12 +1,12 @@
 <?php
-   // $hostname = "localhost";
-    // $db_username = "root";
-    // $db_password = "sugam@123";
-    // $db_name = "farmer_profiles";
-    $hostname = "sql110.infinityfree.com";
-    $db_username = "if0_37166812";
-    $db_password = "MKTbguBxFc5wf";
-    $db_name = "if0_37166812_farmer_profiles";
+  $hostname = "localhost";
+  $db_username = "root";
+  $db_password = "sugam@123";
+  $db_name = "farmer_profiles";
+  // $hostname = "sql110.infinityfree.com";
+  // $db_username = "if0_37166812";
+  // $db_password = "MKTbguBxFc5wf";
+  // $db_name = "if0_37166812_farmer_profiles";
 
     $conn = mysqli_connect($hostname, $db_username, $db_password, $db_name);
 
